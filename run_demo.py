9e258a3ad2b076b2b643f8e1da7a318fb19de67d
LeoKingzc1 <88300284+LeoKingzc1@users.noreply.github.com>
2023-09-21 23:12:30 +0800

    Update run_demo.py

--- run_demo.py
+++ run_demo.py
@@ -1,13 +1,9 @@
-# from src.create_ui import create_ui
-import sys
-sys.path.append("/content/src")
-from ui import create_ui
-
+from src.create_ui import create_ui
 
 def main():
     demo = create_ui()
     demo.queue()
-    demo.launch()
+    demo.launch(share = True)
 
 
 if __name__ == "__main__":
